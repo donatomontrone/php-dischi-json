c728ea343bb8b03d38c7473013dc4bbe93eae302
donatomontrone <donato.montrone@outlook.com>
2023-01-30 15:19:11 +0100

    add is_null function in server.php

--- server.php
+++ server.php
@@ -1,30 +1,30 @@
 <?php
+
+
+
+//*recuperare i file dal json (sarà una stringa);
 $diskData = file_get_contents('./db/dischi.json');
-// echo $diskData;
+
+
+//*inserendo qui l'header il browser riceverà con risposta dall'header un file json invece che un file html
+//! header('Content-Type: application/json');
+
+//*Assegno ad una variabile la decodifica di un array json in un array di array in php(array associativo dato dal true);
 $diskList = json_decode($diskData, true);
-$result = [];
-
-
-
-if (!isset($_GET['diskIndex'])) {
-    $result = $diskList;
-} else {
-    $result = $diskList[$_GET['diskIndex']];
-}
-
-
-
 
 //verifichiamo se c'è o no, faccio tirare fuori qualcosa
+if (is_null($diskList)) {
+    $result = [];
+} else {
+    $result = [];
+    if (!isset($_GET['diskIndex'])) {
+        $result = $diskList;
+    } else {
+        $result = $diskList[$_GET['diskIndex']];
+    }
+}
+
 //Header application/json
 header('Content-Type: application/json');
 
 echo json_encode($result);
-
-//json_encode
-
-
-// elaborazione dell'array asssociativo
-
-
-//prendere i dati dal server ma non dal json;
